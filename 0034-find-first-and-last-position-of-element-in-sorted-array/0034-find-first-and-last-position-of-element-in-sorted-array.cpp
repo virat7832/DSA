536244cbd1085767da38c0adea451899088de1ff
virat7832 <120581720+virat7832@users.noreply.github.com>
2025-02-19 01:45:57 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
+    int lowerBound(vector<int> &nums, int n, int target) {
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) { 
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int upperBound(vector<int> &nums, int n, int target) {
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) { 
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1, last = -1;
-    int left = 0, right = nums.size() - 1;
+        int n = nums.size(); 
+        int lb = lowerBound(nums, n, target);
+        if (lb == n || nums[lb] != target) return { -1, -1 }; 
 
-    // Find the first occurrence
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            first = mid;
-            right = mid - 1; 
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    left = 0;
-    right = nums.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            last = mid;
-            left = mid + 1; 
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return {first, last};
+        return { lb, upperBound(nums, n, target) - 1 };
     }
 };
