34235b545dcb2bad4ee61ca730efe1cc1840f178
virat7832 <120581720+virat7832@users.noreply.github.com>
2025-05-03 01:39:14 +0530

    Time: 22 ms (35.59%), Space: 22.9 MB (39.15%) - LeetHub

---
+++ 0838-push-dominoes/0838-push-dominoes.cpp
@@ -0,0 +1,69 @@
+class Solution 
+{
+public:
+    string pushDominoes(string dominoes) 
+    {
+        int n = dominoes.length();
+        vector<int> forces(n, 0);
+        int force = 0;
+
+        // Step 1: Traverse left to right
+        for (int i = 0; i < n; ++i) 
+        {
+            if (dominoes[i] == 'R') 
+            {
+                force = n;
+            } 
+            else if (dominoes[i] == 'L') 
+            {
+                force = 0;
+            } 
+            else 
+            {
+                force = max(force - 1, 0);
+            }
+            
+            forces[i] += force;
+        }
+
+        // Step 2: Traverse right to left
+        force = 0;
+        for (int i = n - 1; i >= 0; --i) 
+        {
+            if (dominoes[i] == 'L') 
+            {
+                force = n;
+            } 
+            else if (dominoes[i] == 'R') 
+            {
+                force = 0;
+            } 
+            else 
+            {
+                force = max(force - 1, 0);
+            }
+            
+            forces[i] -= force;
+        }
+
+        // Step 3: Build result string
+        string result;
+        for (int f : forces) 
+        {
+            if (f > 0)
+            {
+                result += 'R';
+            } 
+            else if (f < 0)
+            {
+                result += 'L';
+            } 
+            else
+            {
+                result += '.';
+            } 
+        }
+
+        return result;
+    }
+};
