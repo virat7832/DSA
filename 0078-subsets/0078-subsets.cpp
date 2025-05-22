a937e612416a39fb2b5adfd9f0143cf590873743
virat7832 <120581720+virat7832@users.noreply.github.com>
2025-05-22 12:01:35 +0530

    Time: 5 ms (5.95%), Space: 15.5 MB (20.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,21 @@
 class Solution {
-public:
-vector<vector<int>>ans;
-void addSubsets(vector<int>&nums,int i,vector<int>temp){
-    if(i<nums.size()){
- temp.push_back(nums[i]);
-addSubsets(nums,i+1,temp);
-temp.pop_back();
-addSubsets(nums,i+1,temp);
-    } else{
-        return ans.push_back(temp);
-    }
+    void func(int ind,vector<int>&nums,int n,vector<int>ds,vector<vector<int>>&ans){
+if(ind==n){
+    ans.push_back(ds);
+    return;
 }
+ds.push_back(nums[ind]);
+func(ind+1,nums,n,ds,ans);
+ds.pop_back();
+func(ind + 1, nums, n, ds, ans);
+
+    }
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        addSubsets(nums,0,{});
+        int n=nums.size();
+        vector<vector<int>>ans;
+        vector<int>ds;
+        func(0,nums,n,ds,ans);
         return ans;
     }
 };
