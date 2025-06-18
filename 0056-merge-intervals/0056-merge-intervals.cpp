1495a2666b56e851594ea056ddceee22c38ef9c6
virat7832 <120581720+virat7832@users.noreply.github.com>
2025-06-18 11:15:32 +0530

    Time: 3 ms (89.67%), Space: 23.7 MB (80.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,22 +5,11 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) {
-                continue;
-            }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end) {
-                    end=max(end,intervals[j][1]);
-                }
-             else{
-                break;
+            if(ans.empty() ||intervals[i][0] > ans.back()[1] ) ans.push_back(intervals[i]);
+            else {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
         }
-        ans.push_back({start,end});
-        }
-
         return ans;
     }
 };
