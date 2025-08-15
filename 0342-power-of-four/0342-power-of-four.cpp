ace21c9d91a050f6adc2233bb5e897b380937ab1
virat7832 <120581720+virat7832@users.noreply.github.com>
2025-08-16 00:58:59 +0530

    Time: 0 ms (100%), Space: 8 MB (4.79%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,12 @@
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0)
+            return false;
+        double x = log(n) / log(4);
+        return floor(x) == x;
+    }
+};
+
